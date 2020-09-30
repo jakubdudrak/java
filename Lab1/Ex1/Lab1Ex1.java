d1632f59959e6fb402a71fffd92bb3902b3750fa
jakubdudrak <72131724+jakubdudrak@users.noreply.github.com>
2020-09-30 10:37:24 +0100

    Add files via upload

---
+++ Lab1/Ex1/Lab1Ex1.java
@@ -0,0 +1,10 @@
+//Lab1Ex1.java
+//30/09/2020
+//Prints the message My name is Paul and I live in Cork
+
+public class Lab1Ex1 {
+	public static void main(String args[])
+	{
+		System.out.println("My name is Paul and I live in Cork");
+	}
+}
---
+++ Lab1/Ex2/Lab1Ex2.java
@@ -0,0 +1,8 @@
+//Lab1Ex2.java
+//30/09/2020
+
+public class Lab1Ex2 {
+	public static void main(String args[]){
+		System.out.println("\"Fear is the path to the dark side \nFear leads to anger. \nAnger leads to hate \nHate leads to suffering.\" \n\tMaster Yoda");
+	}
+}
